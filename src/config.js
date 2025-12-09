5cf342bb85ee07be525c239ef98d5f35e1c2ae8b
Peter <skwortsow.petia@yandex.ru>
2025-12-09 17:28:24 +0300

    Вторая попытка

--- src/config.js
+++
@@ -1,8 +0,0 @@
-const getBaseUrl = () => {
-    if (window.location.hostname.includes('xn--')) {
-        return '/'; // для IDN доменов
-    }
-    return process.env.PUBLIC_URL || '/';
-};
-
-export const BASE_URL = getBaseUrl();
